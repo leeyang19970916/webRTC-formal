7e31d75bc9b13cfbf72db430bc61d3d50cb59557
leeyang19970916 <yang19970916@gmail.com>
2022-09-20 17:16:31 +0800

    easdfas

--- all.js
+++ all.js
@@ -1,6 +1,10 @@
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+function reportWindowSize() {
+	let vh = window.innerHeight * 0.01;
+	// Then we set the value in the --vh custom property to the root of the document
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+  window.onresize = reportWindowSize;
 async function screenshot() {
 	let canvasWidth = 480
 	// *2是因為2張 remote與local
