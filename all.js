4b1fb1f4f0cc85675e053314f84ec1b45c37a5de
leeyang19970916 <yang19970916@gmail.com>
2022-09-22 12:04:54 +0800

    wwdeqw

--- all.js
+++ all.js
@@ -3,7 +3,7 @@ function reportWindowSize() {
 	let vh =  $(window).height() * 0.01;
 	let aaa = window.innerHeight * 0.01;
 	// Then we set the value in the --vh custom property to the root of the document
-	document.documentElement.style.setProperty('--vh', `${vh}px`);
+	document.documentElement.style.setProperty('--vh', `${aaa}px`);
 	console.log(vh, "reportWindowSize",aaa)
 }
 window.onresize = reportWindowSize;
